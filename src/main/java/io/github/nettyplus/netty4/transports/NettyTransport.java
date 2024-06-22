53f8cc0cce215653e2f1482af87dab16c2314b1d
sullis <github@seansullivan.com>
2024-06-21 21:21:47 -0700

    add NettyTransport.java

---
+++ src/main/java/io/github/nettyplus/netty4/transports/NettyTransport.java
@@ -0,0 +1,53 @@
+package io.github.nettyplus.netty4.transports;
+
+import io.netty.channel.IoHandlerFactory;
+import io.netty.channel.epoll.Epoll;
+import io.netty.channel.epoll.EpollIoHandler;
+import io.netty.channel.epoll.EpollServerSocketChannel;
+import io.netty.channel.nio.NioIoHandler;
+import io.netty.channel.socket.ServerSocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.uring.IoUring;
+import io.netty.channel.uring.IoUringIoHandler;
+import io.netty.channel.uring.IoUringServerSocketChannel;
+import java.util.Arrays;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public enum NettyTransport {
+    NIO(() -> true, NioIoHandler::newFactory, NioServerSocketChannel.class),
+    EPOLL(Epoll::isAvailable, EpollIoHandler::newFactory, EpollServerSocketChannel.class),
+    IO_URING(IoUring::isAvailable, IoUringIoHandler::newFactory, IoUringServerSocketChannel.class);
+    /*
+    KQUEUE(KQueue::isAvailable, KQueueIoHandler::newFactory, KQueueServerSocketChannel.class);
+     */
+
+    private static final NettyTransport[] ALL_VALUES = values();
+
+    private final Supplier<Boolean> isAvailableSupplier;
+    private final Supplier<IoHandlerFactory> ioHandlerFactorySupplier;
+    private final Class<? extends ServerSocketChannel> serverSocketChannelClass;
+
+    NettyTransport(Supplier<Boolean> isAvailable, Supplier<IoHandlerFactory> ioHandlerFactory, Class<? extends ServerSocketChannel> serverSocketClass) {
+        this.isAvailableSupplier = isAvailable;
+        this.ioHandlerFactorySupplier= ioHandlerFactory;
+        this.serverSocketChannelClass = serverSocketClass;
+    }
+
+    public boolean isAvailable() {
+        return isAvailableSupplier.get();
+    }
+
+    public Class<? extends ServerSocketChannel> getServerSocketChannelClass() {
+        return this.serverSocketChannelClass;
+    }
+
+    public IoHandlerFactory createIoHandlerFactory() {
+        return this.ioHandlerFactorySupplier.get();
+    }
+
+    public static Stream<NettyTransport> availableTransports() {
+        return Arrays.stream(ALL_VALUES)
+                .filter(NettyTransport::isAvailable);
+    }
+}
---
+++ src/test/java/io/github/nettyplus/netty4/transports/NettyTransportTest.java
@@ -0,0 +1,48 @@
+package io.github.nettyplus.netty4.transports;
+
+import io.netty.channel.epoll.Epoll;
+import io.netty.channel.uring.IoUring;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class NettyTransportTest {
+    @Test
+    public void nioIsAvailable() {
+        assertTrue(NettyTransport.NIO.isAvailable());
+    }
+
+    @Test
+    @EnabledOnOs(value = { OS.LINUX } )
+    public void epollIsAvailableOnLinux() {
+        assertTrue(Epoll.isAvailable());
+        assertTrue(NettyTransport.EPOLL.isAvailable());
+    }
+
+    @Test
+    @EnabledOnOs(value = { OS.LINUX } )
+    public void ioUringIsAvailableOnLinux() {
+        assertTrue(IoUring.isAvailable());
+        assertTrue(NettyTransport.IO_URING.isAvailable());
+    }
+
+    @Test
+    @EnabledOnOs(value = { OS.LINUX } )
+    public void linuxTransports() {
+        assertEquals(Set.of(NettyTransport.NIO, NettyTransport.IO_URING, NettyTransport.EPOLL),
+                NettyTransport.availableTransports().collect(Collectors.toSet()));
+    }
+
+    @Test
+    @EnabledOnOs(value = { OS.MAC } )
+    public void macTransports() {
+        assertEquals(Set.of(NettyTransport.NIO),
+                NettyTransport.availableTransports().collect(Collectors.toSet()));
+    }
+}
